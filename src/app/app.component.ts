796f6451227064c68630a3d1029c289a94194728
ivanam <iva.moyano@gmail.com>
2020-04-03 16:02:07 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  nombre    : string = 'Capitan America';
+  nombre2: string = 'IvanA MOyano';
+  arreglo   = [1,2,5,6,7,8,9];
+  PI        : number = Math.PI;
+  porcentaje: number = 0.234;
+  salario   :number= 12345;
+  fecha:Date = new Date;
+  idioma: string = 'es';
+  videoUrl:string ='https://www.youtube.com/embed/QJVn00sfNjM';
+
+
+  valorPromesa= new Promise<string>((resolve) =>{
+    setTimeout(() =>{
+    resolve('llegó la data');
+    },4500);
+  })
+  heroe= {
+    nombre:'logan',
+    clave:'wolverine',
+    edad:500,
+    direccion:{
+      calle:'primera',
+      casa:20
+    }
+  }
 }
